8ded8a1e4e2f83f0071634404c642892393fc4e3
yuhui2001 <siaokiabj4@hotmail.com>
2023-11-12 03:21:08 +0800

    semi done announcement
    im good

---
+++ lib/Controller/AnnouncementController.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AnnouncementController {
+  final CollectionReference<Map<String, dynamic>> collection =
+      FirebaseFirestore.instance.collection("Announcement");
+
+  Future<List<Map<String, dynamic>>> getAnnouncements() async {
+    try {
+      QuerySnapshot<Map<String, dynamic>> querySnapshot =
+          await collection.orderBy("Announcement_ID", descending: true).get();
+
+      return querySnapshot.docs.map((doc) => doc.data()).toList();
+    } catch (e) {
+      print("Error getting announcements: $e");
+      return [];
+    }
+  }
+
+  List<String> getTypes() {
+    return ['Random', 'Security'];
+  }
+}
--- lib/View/AnnouncementPage.dart
+++ lib/View/AnnouncementPage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:residential_management_app/View/HomePage.dart';
 import 'package:residential_management_app/View/ProfilePage.dart';
+import 'package:residential_management_app/Controller/AnnouncementController.dart';
+import 'AnnouncementContentPage.dart';
 
 class AnnouncementPage extends StatefulWidget {
   @override
@@ -8,7 +10,10 @@ class AnnouncementPage extends StatefulWidget {
 }
 
 class _AnnouncementPageState extends State<AnnouncementPage> {
-  int _selectedIndex = 1;
+  final int _selectedIndex = 1;
+  final AnnouncementController announcementController =
+      AnnouncementController();
+  String? selectedType;
 
   void _onItemTapped(int index) {
     if (index !=
@@ -33,23 +38,119 @@ class _AnnouncementPageState extends State<AnnouncementPage> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    selectedType = null; // Set the default filter to null (All)
+  }
+
+  @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         title: Center(
-            child: Text(
-          "Anouncement",
-          style: TextStyle(fontSize: 30),
-        )),
+          child: Text(
+            "Announcement",
+            style: TextStyle(fontSize: 30),
+          ),
+        ),
         automaticallyImplyLeading: false,
       ),
-      body: Center(
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Text("Hello, this is announcement page"),
-      ])),
+      body: SingleChildScrollView(
+        child: FutureBuilder<List<Map<String, dynamic>>>(
+          future: announcementController.getAnnouncements(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            }
+
+            List<Map<String, dynamic>> announcements = snapshot.data ?? [];
+
+            // Filter announcements based on selected type
+            List<Map<String, dynamic>> filteredAnnouncements =
+                selectedType == null
+                    ? announcements
+                    : announcements
+                        .where((announcement) =>
+                            announcement['Announcement_Type'] == selectedType)
+                        .toList();
+
+            return Column(
+              children: [
+                // Dropdown for filtering by type
+                DropdownButton<String>(
+                  hint: Text('Filter by Type'),
+                  value: selectedType,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedType = value == 'All' ? null : value;
+                    });
+                  },
+                  items:
+                      ['All', ...announcementController.getTypes()].map((type) {
+                    return DropdownMenuItem<String>(
+                      value: type == 'All' ? null : type,
+                      child: Text(type),
+                    );
+                  }).toList(),
+                ),
+
+                // Display filtered announcements
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: filteredAnnouncements.length,
+                  itemBuilder: (context, index) {
+                    Map<String, dynamic> data = filteredAnnouncements[index];
+                    return Column(
+                      children: [
+                        ListTile(
+                          title: Text('Title: ${data['Announcement_Title']}'),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Type: ${data['Announcement_Type']}'),
+                            ],
+                          ),
+                          trailing: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      AnnouncementContentPage(),
+                                ),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              fixedSize:
+                                  Size(screenWidth * 0.3, screenHeight * 0.1),
+                            ),
+                            child: Text(
+                              'View',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 16), // Adjust spacing as needed
+                      ],
+                    );
+                  },
+                ),
+              ],
+            );
+          },
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
